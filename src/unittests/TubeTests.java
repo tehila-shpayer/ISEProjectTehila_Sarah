f67ff70c1c554a99df0e10ba946b84a8994844d7
tehila1742 <tehila1742@gmail.com>
2021-04-09 13:31:44 +0300

    tube tests

--- src/unittests/TubeTests.java
+++ src/unittests/TubeTests.java
@@ -16,7 +16,7 @@ public class TubeTests {
 		
         // ============ Equivalence Partitions Tests ==============
         // TC01: There is a simple single test here
-		Tube tube = new Tube(new Ray(new Point3D(0,  0, 1), new Vector(0, 0, 1)), 1);
+		Tube tube = new Tube(new Ray(new Point3D(0, 0, 1), new Vector(0, 0, 1)), 1);
 		assertEquals("Bad normal to tube", new Vector(0, 1, 0), tube.getNormal(new Point3D(4, 1, 1)));	
 		
 		// ============ Boundary Partitions Tests ==============
