2dc724ed5e7f0258f171e9e0c973dd3abe89ae0d
Sarah Malka <sarahmal@g.jct.ac.il>
2021-04-09 13:37:30 +0300

    cy

--- src/unittests/CylinderTests.java
+++ src/unittests/CylinderTests.java
@@ -1,4 +1,8 @@
 package unittests;
+import geometries.*;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +12,9 @@ public class CylinderTests {
 
 	@Test
 	public void testGetNormal() {
-		fail("Not yet implemented");
+		Ray ray = new Ray(new Point3D(0, 0, 0), new Vector(new Point3D(0,0,1)));
+		Cylinder cylinder = new Cylinder(ray, 2, 10);
+		assertTrue("Bad normal to cylinder when the point is on the", cylinder.getNormal(new Point3D(0.2, 0.4, 0)).equals(new Vector(0,0,1)) || cylinder.getNormal(new Point3D(0.2, 0.4, 0)).equals(new Vector(0,0,-1)));
 	}
 
 }
