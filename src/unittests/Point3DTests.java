f6c8777b7b05ae33d99bdc5cee4062b2fa4b84c1
tehila1742 <tehila1742@gmail.com>
2021-04-09 10:22:17 +0300

    point3d tests

---
+++ src/unittests/Point3DTests.java
@@ -0,0 +1,33 @@
+package unittests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import primitives.Point3D;
+public class Point3DTests {
+
+	@Test
+	public void testPoint3D() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistanceSquared() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistance() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/unittests/VectorTests.java
+++ src/unittests/VectorTests.java
@@ -1,9 +1,13 @@
 package unittests;
 
 import static org.junit.Assert.*;
-
+import primitives.Vector;
+import primitives.Util;
 import org.junit.Test;
-
+/**
+ * Unit tests for primitives.Vector class
+ * @authors Tehila Shpayer 325236594 and Sarah Malka Hamou 325266401
+ */
 public class VectorTests {
 
 	@Test
@@ -26,10 +30,36 @@ public class VectorTests {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testCrossProduct() {
-		fail("Not yet implemented");
-	}
+	/**
+     * Test method for {@link primitives.Vector#crossProduct(primitives.Vector)}.
+     */
+    @Test
+    public void testCrossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+        Vector v2 = new Vector(0, 3, -2);
+        Vector vr = v1.crossProduct(v2);
+
+        // TC01: Test that length of cross-product is proper (orthogonal vectors taken
+        // for simplicity)
+        assertEquals("crossProduct() wrong result length", v1.length() * v2.length(), vr.length(), 0.00001);
+
+        // TC02: Test cross-product result orthogonality to its operands
+        assertTrue("crossProduct() result is not orthogonal to 1st operand", isZero(vr.dotProduct(v1)));
+        assertTrue("crossProduct() result is not orthogonal to 2nd operand", isZero(vr.dotProduct(v2)));
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test zero vector from cross-productof co-lined vectors
+        Vector v3 = new Vector(-2, -4, -6);
+        assertThrows("crossProduct() for parallel vectors does not throw an exception",
+                IllegalArgumentException.class, () -> v1.crossProduct(v3));
+        // try {
+        //     v1.crossProduct(v2);
+        //     fail("crossProduct() for parallel vectors does not throw an exception");
+        // } catch (Exception e) {}
+    }
+
 
 	@Test
 	public void testLengthSquared() {
