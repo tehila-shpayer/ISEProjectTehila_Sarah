ab7bfe79b601145bdc1878535c1da0b9ed0c4041
tehila1742 <tehila1742@gmail.com>
2021-04-29 16:45:00 +0300

    add given files

---
+++ src/unittests/ImageWriterTest.java
@@ -0,0 +1,24 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImageWriterTest {
+
+	@Test
+	public void testImageWriter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteToImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWritePixel() {
+		fail("Not yet implemented");
+	}
+
+}
