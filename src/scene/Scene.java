053c3b8b89c897c5ba4f0facf51e3156bdb69bd4
Sarah Malka <sarahmal@g.jct.ac.il>
2021-05-02 13:42:27 +0300

    java doc scene

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -1,5 +1,13 @@
 package scene;
-
+/**
+ * Scene class - combines all the objects needed for the scene:
+ * name
+ * background
+ * ambient lighting
+ * the geometry model
+ * 
+ * @authors Tehila Shpayer 325236594 and Sarah Malka Hamou 325266401
+ */
 import elements.AmbientLight;
 import geometries.Geometries;
 import geometries.Geometry;
@@ -17,22 +25,47 @@ public class Scene {
 		geometries = new Geometries();
 	}
 	
+	
+	// ***************** Setters ********************** //
+	// ** all setters implements the Builder Design Pattern **//
+	
+		/**
+		 * set the Background color for the scene
+		 * 
+		 * @return the scene object
+		 */
 	public Scene setBackground(Color _background) {
 		this.background = _background;
 		return this;
 	}
 	
+	/**
+	 * set the Ambient Light for the scene
+	 * 
+	 * @return the scene object
+	 */
 	public Scene setAmbientLight(AmbientLight _ambientLight) {
 		this.ambientLight = _ambientLight;
 		return this;
 	}
 	
+	/**
+	 * set the geometry model - a list of geometries
+	 * 
+	 * @return the scene object
+	 */
 	public Scene setGeometries(Geometries _geometries) {
 		this.geometries = _geometries;
 		return this;
 	}
 	
-	public Scene setGeometries(Intersectable _geometry) {
+
+	/**
+	 * 
+	 * @param _geometry - the new geometry to add to the scene
+	 * @return the scene object
+	 */
+	public Scene addGeometry(Intersectable _geometry) {
 		if(_geometry != null)
 			this.geometries.add(_geometry);
 		return this;
