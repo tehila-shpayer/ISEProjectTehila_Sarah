bb0f186e1c5392046783ab4e162309dce825fae0
tehila1742 <tehila1742@gmail.com>
2021-05-23 23:10:00 +0300

    fds

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,7 +2,6 @@ package geometries;
 
 import java.util.List;
 
-
 import static primitives.Util.*;
 
 /**
@@ -79,42 +78,23 @@ public class Sphere extends Geometry
 		double th = alignZero(Math.sqrt(radius*radius - d*d));
 		double t1 = alignZero(tm - th);
 		double t2 = alignZero(tm + th);
-		
-		List<GeoPoint> res;
-		if(t1 <= 0 && t2 <= 0 )
-			return null;
-		if(t1 <= 0 && t2 > 0)
-			res = List.of(new GeoPoint(this,ray.getPoint(t2)));
-			
-		if(t2 <= 0 && t1 > 0)
-			res = List.of(new GeoPoint(this,ray.getPoint(t1)));
-		res =  List.of(new GeoPoint(this,ray.getPoint(t1)), new GeoPoint(this,ray.getPoint(t2)));
-		
-		for (int i = 0; i<res.size(); i++)
-		{
-			if (alignZero(res.get(i).point.distance(rayQ0) - maxDistance) > 0)
-				res.remove(i);
-		}
-		return res;
 		//A list of conditions that verify that the points are indeed intersection points and are within the desired range
-		/**if((t1 <= 0 && t2 <= 0 )&&((alignZero(t1 - maxDistance) > 0) && (alignZero(t2 - maxDistance) > 0)))
 		if((t1 <= 0 )&& ( t2 <= 0 ))
 			return null;
-		if((t1 <= 0 && t2 > 0)&&((alignZero(t1 - maxDistance) > 0) && (alignZero(t2 - maxDistance) <= 0)))
+		if((t1 <= 0)&&( t2 > 0 && (alignZero(t2 - maxDistance) <= 0)))
 			return List.of(new GeoPoint(this,ray.getPoint(t2)));
-		if((t2 <= 0 && t1 > 0)&&((alignZero(t1 - maxDistance) <= 0) && (alignZero(t2 - maxDistance) > 0)))
+		if((t1 > 0 && (alignZero(t1 - maxDistance) <= 0))&&( t2 <= 0 ))
 			return List.of(new GeoPoint(this,ray.getPoint(t1)));
-		if((t2 > 0 && t1 > 0)&&((alignZero(t1 - maxDistance) <= 0) && (alignZero(t2 - maxDistance) <= 0))) {
+		if((t1 > 0 && (alignZero(t1 - maxDistance) <= 0))&&( t2 > 0 && (alignZero(t2 - maxDistance) <= 0)))
 			return List.of(new GeoPoint(this,ray.getPoint(t1)), new GeoPoint(this,ray.getPoint(t2)));	
-		}
-		return null;*/
+		return null;
 	}
 	
 	@Override
 	public List<GeoPoint> findGeoIntersections(Ray ray){
 		Point3D rayQ0 = ray.getQ0();
 		Vector rayDir = ray.getDir();
-		if(rayQ0.equals(center))
+		if(rayQ0.equals(center)) 
 			return List.of(new GeoPoint(this, ray.getPoint(radius)));
 		Vector q0ToCenter = center.subtract(rayQ0);
 		double tm = alignZero(rayDir.dotProduct(q0ToCenter)); ;
